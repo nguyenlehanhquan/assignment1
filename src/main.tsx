6fede331be2f87b6d7282f50d12b6de2e604de44
Hanh Quan <nguyenlehanhquan@gmail.com>
2025-10-18 19:16:04 +0700

    before vercel deploy

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ import Home from "./pages/Home.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      {/*<AuthProvider>*/}
+      <AuthProvider>
           <BrowserRouter>
               <Routes>
                   <Route path="/login2" element={<Login2 />} />
@@ -37,6 +37,6 @@ createRoot(document.getElementById('root')!).render(
                   <Route path="/profile" element={<Profile />}/>
               </Routes>
           </BrowserRouter>
-      {/*</AuthProvider>*/}
+      </AuthProvider>
   </StrictMode>,
 );
